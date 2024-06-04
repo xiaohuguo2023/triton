da1da4cf77f9c4c415ff84682d2a907d869b6f60
Xiaohu Guo <Xiaohu.Guo@amd.com>
2024-06-04 16:34:38 -0500

    add new do_bench1 to use cpu timer

--- python/triton/testing.py
+++ python/triton/testing.py
@@ -88,6 +88,95 @@ def get_time_us_sync():
     duration = now * 1e6  # Convert seconds to microseconds
     return duration
 
+def do_bench1(fn, warmup=25, rep=100, grad_to_none=None, quantiles=None, fast_flush=True, return_mode="mean"):
+    """
+    Benchmark the runtime of the provided function using both CPU and GPU timers.
+
+    :param fn: Function to benchmark
+    :type fn: Callable
+    :param warmup: Warmup time (in ms)
+    :type warmup: int
+    :param rep: Repetition time (in ms)
+    :type rep: int
+    :param grad_to_none: Reset the gradient of the provided tensor to None
+    :type grad_to_none: torch.tensor, optional
+    :param quantiles: Performance percentile to return in addition to the median.
+    :type quantiles: list[float]
+    :param fast_flush: Use faster kernel to flush L2 between measurements
+    :type fast_flush: bool
+    :param return_mode: Summary statistic to return ("min", "max", "mean", "median")
+    :type return_mode: str
+    """
+    assert return_mode in ["min", "max", "mean", "median"]
+
+    fn()
+    torch.cuda.synchronize()
+
+    # We maintain a buffer of 256 MB that we clear
+    # before each kernel call to make sure that the L2
+    # doesn't contain any input data before the run
+    if fast_flush:
+        cache = torch.empty(int(256e6 // 4), dtype=torch.int, device='cuda')
+    else:
+        cache = torch.empty(int(256e6), dtype=torch.int8, device='cuda')
+
+    # Estimate the runtime of the function
+    start_event = torch.cuda.Event(enable_timing=True)
+    end_event = torch.cuda.Event(enable_timing=True)
+    start_event.record()
+    for _ in range(5):
+        cache.zero_()
+        fn()
+    end_event.record()
+    torch.cuda.synchronize()
+    estimate_ms = start_event.elapsed_time(end_event) / 5
+
+    # compute number of warmup and repeat
+    n_warmup = max(1, int(warmup / estimate_ms))
+    n_repeat = max(1, int(rep / estimate_ms))
+#    start_event = [torch.cuda.Event(enable_timing=True) for i in range(n_repeat)]
+#    end_event = [torch.cuda.Event(enable_timing=True) for i in range(n_repeat)]
+
+    # Warm-up
+    for _ in range(n_warmup):
+        fn()
+
+    # Benchmark
+    cpu_times = []
+    gpu_times = []
+    for i in range(n_repeat):
+        if grad_to_none is not None:
+            for x in grad_to_none:
+                x.grad = None
+        cache.zero_()
+
+        # CPU timer start
+        start_cpu = time.perf_counter()
+
+        # GPU timer start
+    #    start_event[i].record()
+        fn()
+    #    end_event[i].record()
+
+    #    # CPU timer end
+        end_cpu = time.perf_counter()
+        cpu_times.append(end_cpu - start_cpu)
+
+    # Record clocks
+    torch.cuda.synchronize()
+#    gpu_times = [s.elapsed_time(e) / 1000 for s, e in zip(start_event, end_event)]  # convert ms to seconds
+    gpu_times = cpu_times
+
+    if quantiles is not None:
+        cpu_quantiles = torch.quantile(torch.tensor(cpu_times, dtype=torch.float), torch.tensor(quantiles, dtype=torch.float)).tolist()
+        gpu_quantiles = torch.quantile(torch.tensor(gpu_times, dtype=torch.float), torch.tensor(quantiles, dtype=torch.float)).tolist()
+        return cpu_quantiles, gpu_quantiles
+
+    cpu_time = getattr(torch, return_mode)(torch.tensor(cpu_times)).item()
+    gpu_time = getattr(torch, return_mode)(torch.tensor(gpu_times)).item()
+
+    return cpu_time, gpu_time
+
 def do_bench(fn, warmup=2, rep=10, grad_to_none=None, quantiles=None, fast_flush=True, return_mode="mean"):
     """
     Benchmark the runtime of the provided function. By default, return the median runtime of :code:`fn` along with
@@ -108,7 +197,6 @@ def do_bench(fn, warmup=2, rep=10, grad_to_none=None, quantiles=None, fast_flush
     """
     assert return_mode in ["min", "max", "mean", "median"]
 
-    fn()
     torch.cuda.synchronize()
 
     # We maintain a buffer of 256 MB that we clear
