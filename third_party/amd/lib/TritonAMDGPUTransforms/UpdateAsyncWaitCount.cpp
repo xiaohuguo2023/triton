8e2fa8b579d45feb1a5a3408b2989a808d3f6f7e
Xiaohu Guo <Xiaohu.Guo@amd.com>
2026-04-27 12:53:13 -0500

    Revert "Use asyncmark/wait_asyncmark for CDNA3/CDNA4 buffer_load_to_lds"
    This reverts commit 10292eae06e9d968985b3588fc79a0313fc8615e.

--- third_party/amd/lib/TritonAMDGPUTransforms/UpdateAsyncWaitCount.cpp
+++ third_party/amd/lib/TritonAMDGPUTransforms/UpdateAsyncWaitCount.cpp
@@ -434,22 +434,9 @@ struct TritonAMDGPUUpdateAsyncWaitCountPass
     };
 
     // Note: AsyncWaits should ignore TDM ops; different HW counter
-    bool useAsyncMarks = llvm::is_contained(
-        {triton::AMD::ISAFamily::CDNA3, triton::AMD::ISAFamily::CDNA4},
-        targetInfo.getISAFamily());
-
     for (auto waitOp : waitOps) {
       IRRewriter builder(waitOp->getContext());
-      if (useAsyncMarks) {
-        // With asyncmark/wait_asyncmark, LLVM handles vmcnt computation.
-        // Pass through the commit group count directly.
-        auto tokens = waitOp.getAsyncToken();
-        builder.setInsertionPointAfter(waitOp);
-        builder.replaceOpWithNewOp<amdgpu::AsyncWaitOp>(waitOp, tokens,
-                                                        waitOp.getNum());
-      } else {
-        updateWaitCount(waitOp, countAsyncLoadInstructions, builder);
-      }
+      updateWaitCount(waitOp, countAsyncLoadInstructions, builder);
     }
 
     // amdgpu.AsyncTDMWait should only count async tdm ops
